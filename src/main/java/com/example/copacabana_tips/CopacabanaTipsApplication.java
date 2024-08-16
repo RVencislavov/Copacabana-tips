c2d68cb2fb330814d09449e1d7ae697686c9d197
Radoslav Georgiev <radoslavasz@abv.bg>
2024-08-16 14:12:21 +0300

    Project which will be used as a portal for the employees at copacabana-tips . Initial commit

---
+++ src/main/java/com/example/copacabana_tips/CopacabanaTipsApplication.java
@@ -0,0 +1,13 @@
+package com.example.copacabana_tips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopacabanaTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopacabanaTipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/copacabana_tips/CopacabanaTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.copacabana_tips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopacabanaTipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
